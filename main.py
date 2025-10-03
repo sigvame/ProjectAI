dd933726018b5f2453972ff8cea425bc2901e8cd
sigvame <157107580+sigvame@users.noreply.github.com>
2025-10-03 15:41:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,127 +1,186 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
 
 
+#завдання 1
 
-# завдання 1
-
-data = {
-    "Назва": ["Дюна", "Атлас розправляє плечі", "Гаррі Поттер",
-              "Володар перснів", "1984", "Автостопом по галактиці"],
-    "Автор": ["Френк Герберт", "Айн Ренд", "Джоан Роулінг",
-              "Дж. Р. Р. Толкін", "Джордж Орвелл", "Дуглас Адамс"],
-    "Рік видання": [1965, 1957, 1997, 1954, 1949, 1979],
-    "Ціна": [350, 480, 400, 520, 290, 310]
-}
-
-df = pd.DataFrame(data)
-
-print("DataFrame:")
-print(df)
-print("-" * 30)
-
-average_price = df["Ціна"].mean()
-print(f"Середня ціна книг: {average_price:.2f} грн")
-print("-" * 30)
-
-r_books = df[df["Рік видання"] > 2015]
-print("Книги, видані після 2015 року:")
-if not r_books.empty:
-    print(r_books)
-else:
-    print("Немає книг після 2015 року")
-print("-" * 30)
-
-sorted_data = df.sort_values(by="Ціна", ascending=True)
-print("")
-print(sorted_data)
-
+# num_days = 30
+# start_date = pd.to_datetime('2025-01-01')
+# dates = pd.date_range(start=start_date, periods=num_days, freq='D')
+# np.random.seed(42)
+#
+# b_users = np.linspace(100, 150, num_days)
+# users = (b_users + np.random.normal(0, 10, num_days)).astype(int)
+# sessions = (users * 1.5 + np.random.normal(0, 15, num_days)).astype(int)
+# r = (sessions * 0.8 + np.random.normal(0, 10, num_days))
+#
+# df = pd.DataFrame({
+#     'date': dates,
+#     'users': users,
+#     'sessions': sessions,
+#     'revenue': r
+# })
+#
+# print("DataFrame:")
+# print(df.head())
+# print("\n" + "=" * 50 + "\n")
+#
+# matrix = df[['users', 'sessions', 'revenue']].corr()
+# print("Кореляційна матриця:")
+# print(matrix)
+# print("\n" + "=" * 50 + "\n")
+#
+# fig, l = plt.subplots(1, 3, figsize=(18, 5))
+# fig.suptitle('Діаграми розсіювання', fontsize=16)
+#
+# # users-sessions
+# l[0].scatter(df['users'], df['sessions'], color='blue', alpha=0.6)
+# l[0].set_title('Користувачі - Сесії')
+# l[0].set_xlabel('Користувачі')
+# l[0].set_ylabel('Сесії')
+# l[0].grid(True, linestyle=':', alpha=0.5)
+#
+# # users-revenue
+# l[1].scatter(df['users'], df['revenue'], color='red', alpha=0.6)
+# l[1].set_title('Користувачі - Виручка')
+# l[1].set_xlabel('Користувачі')
+# l[1].set_ylabel('Виручка')
+# l[1].grid(True, linestyle=':', alpha=0.5)
+#
+# #sessions-revenue
+# l[2].scatter(df['sessions'], df['revenue'], color='green', alpha=0.6)
+# l[2].set_title('Сесії - Виручка')
+# l[2].set_xlabel('Сесії')
+# l[2].set_ylabel('Виручка')
+# l[2].grid(True, linestyle=':', alpha=0.5)
+#
+# plt.tight_layout(rect=[0, 0.03, 1, 0.95], w_pad=4.0)
+# plt.show()
+#
+#
+# plt.figure(figsize=(12, 6))
+# plt.plot(df['date'], df['revenue'], marker='o', linestyle='-', color='purple', label='Виручка')
+#
+# plt.title('Лінійний графік за датами')
+# plt.xlabel('Дата')
+# plt.ylabel('Виручка')
+# plt.xticks(rotation=45)
+# plt.grid(True, linestyle='--', alpha=0.6)
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
 
 
 
 
 #завдання 2
 
-# df = pd.read_csv('orders.csv')
+# N = 200
 #
-# print("Перші 10 рядків даних:")
-# print(df.head(10))
-# print("\n" + "=" * 50 + "\n")
+# data = {
+#     'Group': ['A'] * N + ['B'] * N,
+#     'Converted': np.concatenate([
+#         np.random.choice([0, 1], size=N, p=[0.90, 0.10]), # 10% для A
+#         np.random.choice([0, 1], size=N, p=[0.88, 0.12])  # 12% для B
+#     ])
+# }
 #
-# orders_by_client = df['Клієнт'].value_counts()
-# print("Кількість замовлень кожного клієнта:")
-# print(orders_by_client)
-# print("\n" + "=" * 50 + "\n")
+# df = pd.DataFrame(data)
 #
-# max_sum = df['Сума'].max()
-# min_sum = df['Сума'].min()
-# print(f"Максимальна сума замовлення: {max_sum}")
-# print(f"Мінімальна сума замовлення: {min_sum}")
-# print("\n" + "=" * 50 + "\n")
+# summary = df.groupby('Group')['Converted'].agg(['count', 'sum']).reset_index()
+# summary.columns = ['Group', 'Total', 'Conversions']
+# summary['CR'] = summary['Conversions'] / summary['Total']
 #
-# total_sum = df['Сума'].sum()
-# print(f"Загальна сума всіх замовлень: {total_sum}")
+# cr_A = summary[summary['Group'] == 'A']['CR'].values[0]
+# cr_B = summary[summary['Group'] == 'B']['CR'].values[0]
+# difference = cr_B - cr_A
+# change = difference / cr_A
+#
+# z = stats.norm.ppf(0.975)
+#
+# def calc_ci(row):
+#     p = row['CR']
+#     n = row['Total']
+#     s = np.sqrt((p * (1 - p)) / n)
+#     m = z * s
+#     return pd.Series([p - m, p + m])
+#
+# summary[['CI_L', 'CI_U']] = summary.apply(calc_ci, axis=1)
+#
+# print(summary[['Group', 'Total', 'Conversions', 'CR', 'CI_L', 'CI_U']].to_string(index=False, float_format="%.4f"))
+# print("\n" + "=" * 50)
+# print(f"Абсолютна різниця: {difference:.4f}")
+# print(f"Відносна зміна: {change:.2%}")
+# print("=" * 50 + "\n")
+#
+#
+# plt.figure(figsize=(8, 6))
+# bars = plt.bar(summary['Group'], summary['CR'], color=['#4C72B0', '#DD8452'])
+#
+# plt.ylabel("Конверсія")
+# plt.title("Конверсія груп A та B")
+# plt.ylim(bottom=0, top=summary['CR'].max() * 1.2) # Збільшуємо верхню межу для міток
+# plt.grid(axis='y', linestyle='--', alpha=0.6)
+#
+# for bar in bars:
+#     height = bar.get_height()
+#     plt.text(bar.get_x() + bar.get_width() / 2., height + 0.005,
+#              f'{height:.2%}',
+#              ha='center', va='bottom', fontsize=12, fontweight='bold')
+#
+# plt.show()
 
 
 #завдання 3
 
-# data = {
-#     "Продукт": ["Мигдаль", "Яблуко", "Куряче філе", "Рис", "Вівсянка",
-#                 "Авокадо", "Банан", "Стейк", "Броколі", "Сир твердий"],
-#     "Категорія": ["Горіхи", "Фрукти", "М'ясо", "Зернові", "Зернові",
-#                   "Фрукти", "Фрукти", "М'ясо", "Овочі", "Молочні"],
-#     "Калорії": [576, 52, 165, 130, 389,
-#                 160, 89, 271, 55, 402],
-#     "Білки": [21, 0.3, 31, 2, 16,
-#               2, 1, 26, 3, 25]
-# }
-#
-# df = pd.DataFrame(data)
-#
-# print("DataFrame:")
-# print(df)
-# print("-" * 50)
-#
-# products = df[df["Калорії"] > 300]
-# print("Продукти з калорійністю вище 300:")
-# print(products)
-# print("-" * 50)
-#
-# protein = df.groupby("Категорія")["Білки"].mean()
-# print("Середня кількість білків за категоріями:")
-# print(protein)
-# print("-" * 50)
-#
-# sorted_data = df.sort_values(by="Калорії", ascending=False)
-# print(sorted_data)
-
-
-
-
 #завдання 4
 
-# data = {
-#     "Ім'я": ["Іван", "Микита", "Андрій", "Вікторія", "Микита", "Іван", "Андрій", "Катерина", "Катерина"],
-#     "Проект": ["Проєкт А", "Проєкт Б", "Проєкт В", "Проєкт Б", "Проєкт А", "Проєкт В", "Проєкт А", "Проєкт Б", "Проєкт В"],
-#     "Години": [15, 20, 10, 25, 30, 10, 5, 20, 15]
-# }
-#
-# df = pd.DataFrame(data)
-#
-# print("DataFrame:")
-# print(df)
-# print("-" * 50)
-#
-# employee_hours = df.groupby("Ім'я")["Години"].sum()
-# print("Загальна кількість годин за кожним співробітником:")
-# print(employee_hours)
-# print("-" * 50)
-#
-# project_hours = df.groupby("Проект")["Години"].sum()
-# print("Загальна кількість годин за кожним проектом:")
-# print(project_hours)
-# print("-" * 50)
-#
-# employee_name = employee_hours.idxmax()
-# employee_h = employee_hours.max()
-# print(f"Співробітник, який витратив найбільше годин: {employee_name}")
-# print(f"Кількість годин: {employee_h}")
+NUM_DAYS = 90
+win = 7
+
+start_date = pd.to_datetime('2025-10-01')
+dates = pd.date_range(start=start_date, periods=NUM_DAYS, freq='D')
+np.random.seed(42)
+
+trend = np.linspace(100, 150, NUM_DAYS)
+noise = np.random.normal(0, 10, NUM_DAYS)
+
+sales = trend + noise
+df = pd.DataFrame({
+    'date': dates,
+    'sales': sales
+})
+df.set_index('date', inplace=True)
+df['Rolling_Mean'] = df['sales'].rolling(window=win).mean()
+df['Rolling_Std'] = df['sales'].rolling(window=win).std()
+
+plt.figure(figsize=(12, 6))
+plt.plot(df.index, df['sales'], label='Вихідні продажі', color='lightgrey', alpha=0.8)
+
+plt.plot(df.index, df['Rolling_Mean'], label=f'Ковзне середнє (Вікно {win})', color='blue', linewidth=2)
+
+plt.title(f'Продажі та ковзне середнє (Вікно {win} днів)')
+plt.xlabel('Дата')
+plt.ylabel('Продажі')
+plt.grid(True, linestyle=':', alpha=0.6)
+plt.legend()
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(12, 4))
+plt.plot(df.index, df['Rolling_Std'], label=f'Ковзне стандартне відхилення (Вікно {win})', color='red', linewidth=2)
+
+plt.title(f'Ковзне стандартне відхилення продажів')
+plt.ylabel('Стандартне відхилення')
+plt.grid(True, linestyle=':', alpha=0.6)
+plt.legend()
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+print("\n" + "=" * 50)
+print(df.head(10).to_string(float_format="%.2f"))
+
